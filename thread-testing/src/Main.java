135c5b6350d8abde0369ffd7abedc01ae2f7b41e
Lucas DRAESCHER <draescherl@cy-tech.fr>
2022-02-16 00:56:45 +0000

    First multithreaded application in java.

--- thread-testing/src/Main.java
+++ thread-testing/src/Main.java
@@ -1,6 +1,48 @@
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static int[] initArray() {
+        int[] array = new int[100000000];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(5000);
+        }
+        return array;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int[] array = initArray();
+        Instant start, finish;
+        long timeElapsed;
+        int max;
+
+        System.out.println("Finding max with one thread ...");
+        MaxArray thread = new MaxArray(array, 0, array.length - 1);
+        start = Instant.now();
+        thread.start();
+        thread.join();
+        finish = Instant.now();
+        max = thread.getMax();
+        timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.println(MessageFormat.format("Max found: {0}, took {1} ms.", max, timeElapsed));
+
+        MaxArray thread1 = new MaxArray(array, 0, array.length / 2);
+        MaxArray thread2 = new MaxArray(array, array.length / 2 + 1, array.length - 1);
+        System.out.println("Finding max with two threads ...");
+        start = Instant.now();
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        finish = Instant.now();
+        max = Math.max(thread1.getMax(), thread2.getMax());
+        timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.println(MessageFormat.format("Max found: {0}, took {1} ms.", max, timeElapsed));
+
     }
 }
--- thread-testing/src/MaxArray.java
+++ thread-testing/src/MaxArray.java
@@ -1,8 +1,26 @@
-public class MaxArray implements Runnable {
+public class MaxArray extends Thread {
+
+    private final int startIndex;
+    private final int endIndex;
+    private int max;
+    private final int[] array;
+
+    public MaxArray(int[] array, int a, int b) {
+        this.array = array;
+        this.startIndex = a;
+        this.endIndex = b;
+    }
+
+    public int getMax() {
+        return max;
+    }
 
     @Override
     public void run() {
-
+        max = this.array[this.startIndex];
+        for (int i = (this.startIndex + 1); i <= this.endIndex; i++) {
+            if (this.array[i] > this.max) this.max = this.array[i];
+        }
     }
 
 }
